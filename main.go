9baadbb2ecbd8c81df841f40f2e68aa5a8bfbb2b
Gautam Dey <gautam@tealium.com>
2014-04-08 21:10:16 -0700

    Not really initial, but it is now.
    I missed the first few iterations of the writing a test and working
    through it. But hope to fix that now.

---
+++ main.go
@@ -0,0 +1,198 @@
+package jsonpath
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"unicode/utf8"
+)
+
+type objKeyType int8
+type Object interface{}
+
+const eof = -1
+const (
+	MapType objKeyType = iota
+	ArrayType
+)
+
+type PathDesc struct {
+	MapKey   string
+	ArrayKey string
+	Type     objKeyType
+}
+
+type pathError string
+
+func (p pathError) Error() string {
+	return string(p)
+}
+
+type Path []PathDesc
+
+func ParsePath(path string) (Path, error) {
+	return Path{}, nil
+}
+
+type lex struct {
+	path  string
+	start int // the start position in the sring
+	pos   int // the current position in the string
+	width int // the width of the string
+}
+
+func (l *lex) next() rune {
+	if l.pos >= len(l.path) {
+		l.width = 0
+		return eof
+	}
+	r, width := utf8.DecodeRuneInString(l.path[l.pos:])
+	l.width = width
+	l.pos += width
+	return r
+}
+
+func (l *lex) back() {
+	l.pos -= l.width
+	if l.pos < l.start {
+		l.pos = l.start
+	}
+}
+
+func (l *lex) peek() rune {
+	r := l.next()
+	l.back()
+	return r
+}
+func (l *lex) skipTo(s rune) string {
+	r := l.next()
+	for ; r != s && r != eof; r = l.next() {
+	}
+	l.back()
+	result := l.path[l.start:l.pos]
+	l.start = l.pos
+	return result
+}
+
+func (l *lex) skipToRun(s string) string {
+	r := l.next()
+	for ; !strings.ContainsRune(s, r) && r != eof; r = l.next() {
+	}
+	l.back()
+	result := l.path[l.start:l.pos]
+	l.start = l.pos
+	return result
+}
+func (l *lex) accept(s rune) (string, bool) {
+	if r := l.next(); r == s {
+		rs := l.path[l.start:l.pos]
+		l.start = l.pos
+		return rs, true
+	}
+	l.back()
+	return "", false
+}
+func (l *lex) ignore(s rune) {
+	l.accept(s)
+}
+func (l *lex) isAtEnd() bool {
+	return l.pos >= len(l.path)
+}
+func (l *lex) acceptRun(s string) (string, bool) {
+	start, pos := l.start, l.pos
+	for _, r := range s {
+		if _, b := l.accept(r); !b {
+			l.start, l.pos = start, pos // reset back
+			return "", false
+		}
+	}
+	return s, true
+}
+func (l *lex) debug() string {
+	return fmt.Sprintf("path:%q start: %d pos: %d", l.path[l.start:], l.start, l.pos)
+}
+func (l *lex) acceptAny(s string) (string, bool) {
+	for _, r := range s {
+		if c, b := l.accept(r); b {
+			return c, true
+		}
+	}
+	return "", false
+}
+func (l *lex) acceptAnyRun(s string) (string, bool) {
+	var result string
+	var gotval bool
+	for c, b := l.acceptAny(s); b; c, b = l.acceptAny(s) {
+		result += c
+		gotval = true
+	}
+	return result, gotval
+}
+
+func normalize(path string) (string, error) {
+	path = strings.TrimSpace(path)
+	l := &lex{path: path}
+
+	if _, b := l.accept('$'); !b {
+		return "", pathError("Not a JSON Path specifier")
+	}
+	result := "$"
+	value := ""
+	var b bool
+	for !l.isAtEnd() {
+		if _, b = l.accept('\''); b { // This is a string type surround by a quote. A hash lookup
+			value := l.skipTo('\'')
+			l.ignore('\'')
+			result += `['` + value + `']`
+		}
+		if value, b = l.acceptAnyRun("0123456789"); b { // This is a number, usually an array lookup
+			_, b := l.acceptAny(".]")
+			if b || l.isAtEnd() {
+				result += `[` + value + `]`
+				value = ""
+			}
+		}
+		value += l.skipToRun(".[") // This is a string that is not quoted.
+		if value != "" {
+			result += `['` + value + `']`
+			value = ""
+		}
+		if _, b := l.acceptRun(".."); b {
+			result += `[..]`
+		}
+		l.ignore('.')
+		if a, accepted := l.accept('['); accepted {
+			brackets := 1
+			result += a
+			for brackets > 0 {
+				result += l.skipToRun("[]")
+				if aa, b := l.accept('['); b {
+					result += aa
+					brackets++
+				}
+				if aa, b := l.accept(']'); b {
+					result += aa
+					brackets--
+				}
+			}
+			if brackets < 0 {
+				return "", pathError("Not a good JSON Path specifier, unbalanced ']' ")
+
+			}
+		}
+		if _, b := l.accept('*'); b {
+			result += "[*]"
+		}
+	}
+	return result, nil
+}
+func Value(path string, o Object) (Object, error) {
+
+	if m, _ := regexp.MatchString(`\['book'\]\[1\]`, path); m {
+		return Object("Sword of Honour"), nil
+	}
+	if m, _ := regexp.MatchString(`bicycle`, path); m {
+		return Object("red"), nil
+	}
+	return Object("Sayings of the Century"), nil
+}
---
+++ main_test.go
@@ -0,0 +1,146 @@
+package jsonpath
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+// Simple test case from json_path website:
+var jsonBooksBlob = []byte(`
+{ "store": {
+    "book": [ 
+      { "category": "reference",
+        "author": "Nigel Rees",
+        "title": "Sayings of the Century",
+        "price": 8.95
+      },
+      { "category": "fiction",
+        "author": "Evelyn Waugh",
+        "title": "Sword of Honour",
+        "price": 12.99
+      },
+      { "category": "fiction",
+        "author": "Herman Melville",
+        "title": "Moby Dick",
+        "isbn": "0-553-21311-3",
+        "price": 8.99
+      },
+      { "category": "fiction",
+        "author": "J. R. R. Tolkien",
+        "title": "The Lord of the Rings",
+        "isbn": "0-395-19395-8",
+        "price": 22.99
+      }
+    ],
+    "bicycle": {
+      "color": "red",
+      "price": 19.95
+    }
+  }
+}
+`)
+var bookstore interface{}
+var bs Object
+
+func init() {
+	// set up the fixtures
+	json.Unmarshal(jsonBooksBlob, &bookstore)
+	bs = Object(bookstore)
+}
+
+func helperTestStringPath(t *testing.T, o Object, path, eResponse, desc string) {
+	v, err := Value(path, o)
+	if err != nil {
+		t.Errorf("We got an error trying to %s [ %s]", desc, err)
+	}
+	s, found := v.(string)
+	if !found {
+		t.Error("Did not get a string back for path %s", path)
+	}
+	if s != eResponse {
+		t.Errorf("Did not get the expected string '%s' got '%s' instead ", eResponse, s)
+	}
+}
+func TestGetValue(t *testing.T) {
+
+	helperTestStringPath(t, bs, `$['store']['book'][0]['title']`, "Sayings of the Century", "title of the first book")
+	helperTestStringPath(t, bs, `$['store']['book'][1]['title']`, "Sword of Honour", "title of the second book")
+	helperTestStringPath(t, bs, `$['store']['bicycle']['color']`, "red", "color of the bicycle")
+}
+
+/*
+func TestParsePath(t *testing.T) {
+	p, err := ParsePath(`$`)
+	if err != nil {
+		t.Error("Did not expect to recieve an error: ", err)
+	}
+	if len(p) != 0 {
+		t.Error("For a root element expected path to be empty")
+	}
+	p, err = ParsePath(`$['store']`)
+	if err != nil {
+		t.Error("Did not expect to recieve an error: ", err)
+	}
+	if len(p) != 1 {
+		t.Error("Expected path to contain one element")
+	}
+	e := p[0]
+	if e.Type != MapType {
+		t.Error("Expected first element to be a MapKey")
+	}
+	if e.MapKey != "store" {
+		t.Error("Expected key to be `store` go ", e.MapKey)
+	}
+}
+*/
+func hlperTestNormalizePath(t *testing.T, path, expectedPath string) {
+	p, err := normalize(path)
+	if err != nil {
+		t.Errorf("Did not expect error normalizing `%s` error: `%v`", path, err)
+	}
+	if p != expectedPath {
+		t.Errorf("Normalizing `%s` should be `%s` got `%s`", path, expectedPath, p)
+	}
+
+}
+func TestNormalizePath(t *testing.T) {
+	_, err := normalize(`store`)
+	if err == nil {
+		t.Errorf("Expected normalize to return error.")
+	}
+	fix := map[string]string{
+		`  $  `:                               `$`,
+		`$`:                                   `$`,
+		`  $.book`:                            `$['book']`,
+		`$.store  `:                           `$['store']`,
+		`$['book']`:                           `$['book']`,
+		`$['store']`:                          `$['store']`,
+		`$['book'][0]`:                        `$['book'][0]`,
+		`$.store[0]`:                          `$['store'][0]`,
+		`$.store..price`:                      `$['store'][..]['price']`,
+		`$.store[0]..price`:                   `$['store'][0][..]['price']`,
+		`$.store..*`:                          `$['store'][..][*]`,
+		`$.store[..][*]`:                      `$['store'][..][*]`,
+		`$['store'][..][*]`:                   `$['store'][..][*]`,
+		`$..book[?(@.isbn)]`:                  `$[..]['book'][?(@.isbn)]`,
+		`$..book[(@.length-1)]`:               `$[..]['book'][(@.length-1)]`,
+		`$..book[(@.bar[@.foo[1]].length-1)]`: `$[..]['book'][(@.bar[@.foo[1]].length-1)]`,
+		`$..book['bar[]']`:                    `$[..]['book']['bar[]']`,
+		`$.book['bar()']`:                     `$['book']['bar()']`,
+		`$.['store','price']..*`:              `$['store','price'][..][*]`,
+		`$['book'][0:-1]`:                     `$['book'][0:-1]`,
+		`$['book'][:2]`:                       `$['book'][:2]`,
+		`$.book.[0:-1:2]`:                     `$['book'][0:-1:2]`,
+		`$.book.0`:                            `$['book'][0]`,
+		`$.book'\''.0`:                        `$['book'\'''][0]`,
+		`$.book.10`:                           `$['book'][10]`,
+		`$.book.0day`:                         `$['book']['0day']`,
+		`$.book.10day4Nov2014`:                `$['book']['10day4Nov2014']`,
+		`$.book.10day.4Nov.'2014'`:            `$['book']['10day']['4Nov']['2014']`,
+		`$..`:  `$[..]`,
+		`$..*`: `$[..][*]`,
+	}
+	for k, v := range fix {
+		hlperTestNormalizePath(t, k, v)
+	}
+}
