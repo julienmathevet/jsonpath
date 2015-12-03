a65b1be98eb2e0b32a5655fd3388b2576e5cac1b
Gautam Dey <gautam.dey77@gmail.com>
2015-12-02 16:23:48 -0800

    Got the first set of test to pass.

--- parse.go
+++ parse.go
@@ -1,9 +1,66 @@
 package jsonpath
 
+import "strings"
+
 // The first thing we need to do is transform a dot–notation to a bracket–notation.
 
-func normalize(s string) (string, error) {
-	//bs := strings.NewReader(s)
+func minNotNeg1(a int, bs ...int) int {
+	m := a
+	for _, b := range bs {
+		if a == -1 || (b != -1 && b < m) {
+			m = b
+		}
+	}
+	return m
+}
 
-	return `$["store"]["book"][0]["title"]`, nil
+func normalize(s string) (string, error) {
+	if s == "" || s == "$." {
+		return "$", nil
+	}
+	r := "$"
+	// first thing to do is read passed the $
+	if s[0] == '$' {
+		s = s[1:]
+	}
+	for len(s) > 0 {
+
+		// Grab all the bracketed entries
+		for len(s) > 0 && s[0] == '[' {
+			n := strings.Index(s, "]")
+			r += s[0 : n+1]
+			s = s[n+1:]
+		}
+		if len(s) <= 0 || s == "." {
+			break
+		}
+
+		if s[0] == '.' {
+			if s[1] == '.' {
+				r += "[..]"
+				s = s[2:]
+
+			} else {
+				s = s[1:]
+			}
+		}
+		if s[0] == '*' {
+			r += "[*]"
+			if len(s) == 1 {
+				break
+			}
+			s = s[1:]
+		}
+
+		n := minNotNeg1(strings.Index(s, "["), strings.Index(s, "."))
+
+		if n != -1 {
+			r += `["` + s[:n] + `"]`
+			s = s[n:]
+		} else {
+			r += `["` + s + `"]`
+			s = ""
+		}
+	}
+	return r, nil
 }
--- parse_test.go
+++ parse_test.go
@@ -4,35 +4,39 @@ import "testing"
 
 func TestNormalize(t *testing.T) {
 	var ev string
-	var ee error
+	var err error
 	testcases := []struct {
-		tv string // Test Value
-		ev string // Expected Value
-		ee error  // Expected Error
+		t                 string // Test Value
+		e                 string // Expected Value
+		err               error  // Expected Error
+		DontTestImplicate bool
 	}{
-		{"$.store.book[0].title", `$["store"]["book"][0]["title"]`, nil},
-		/*
-			{"$.store.book[*].author", `$["store"]["book"][*]["author"]`, nil},
-			{"$..author", `$[..]["author"]`, nil},
-			{"$.store.*", `$["store"][*]`, nil},
-			{"$.store.book[?(@.length()-1)].title", `$["store"]["book"][?(@.length()-1)["title"]`, nil},
-		*/
+		{t: "$.store.book[0].title", e: `$["store"]["book"][0]["title"]`},
+		{t: "$.store.book[*].author", e: `$["store"]["book"][*]["author"]`},
+		{t: "$.", e: `$`},
+		{t: ".", e: "$", DontTestImplicate: true},
+		{t: "$..author", e: `$[..]["author"]`, DontTestImplicate: true},
+		{t: "..author", e: `$[..]["author"]`, DontTestImplicate: true},
+		{t: "$.store.*", e: `$["store"][*]`},
+		{t: "$.store.book[?(@.length()-1)].title", e: `$["store"]["book"][?(@.length()-1)]["title"]`},
 	}
 	for i, test := range testcases {
 		// First let's run the normal test.
-		ev, ee = normalize(test.tv)
-		if ev != test.ev || ee != test.ee {
-			t.Errorf("[%03d] Standard test:  Normalize(\"%v\") = \"%v\", %v; want %v, %v", i, test.tv, ev, ee, test.ev, test.ee)
+		ev, err = normalize(test.t)
+		if ev != test.e || err != test.err {
+			t.Errorf("[%03d] Standard test:  Normalize(\"%v\") = \"%v\", %v; want %v, %v", i, test.t, ev, err, test.e, test.err)
 		}
 		// Test that a normalized version is a noop.
-		ev, ee = normalize(test.ev)
-		if ev != test.ev || ee != test.ee {
-			t.Errorf("[%03d] NOOP test:      Normalize(\"%v\") = \"%v\", %v; want %v, %v", i, test.ev, ev, ee, test.ev, test.ee)
+		ev, err = normalize(test.e)
+		if ev != test.e || err != test.err {
+			t.Errorf("[%03d] NOOP test:      Normalize(\"%v\") = \"%v\", %v; want %v, %v", i, test.e, ev, err, test.e, test.err)
 		}
-		// Test that an implicate version works as well.
-		ev, ee = normalize(test.tv[2:])
-		if ev != test.ev || ee != test.ee {
-			t.Errorf("[%03d] Implicate test: Normalize(\"%v\") = \"%v\", %v; want %v, %v", i, test.tv[2:], ev, ee, test.ev, test.ee)
+		if !test.DontTestImplicate {
+			// Test that an implicate version works as well.
+			ev, err = normalize(test.t[2:])
+			if ev != test.e || err != test.err {
+				t.Errorf("[%03d] Implicate test: Normalize(\"%v\") = \"%v\", %v; want %v, %v", i, test.t[2:], ev, err, test.e, test.err)
+			}
 		}
 
 	}
