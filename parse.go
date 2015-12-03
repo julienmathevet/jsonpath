75e654f2c133bd605a1ef8ed4725dfa1f1ab6e66
Gautam Dey <gautam.dey77@gmail.com>
2015-12-03 03:48:03 -0800

    Added the ability to parse JSONPath string.

--- parse.go
+++ parse.go
@@ -1,6 +1,146 @@
 package jsonpath
 
-import "strings"
+import (
+	"errors"
+	"io"
+	"strconv"
+	"strings"
+)
+
+type Applicator interface {
+	Apply(v interface{}) (interface{}, error)
+}
+
+type node interface {
+	Applicator
+	SetNext(v node)
+}
+
+var (
+	MapTypeError      = errors.New("Expected Type to be a Map.")
+	ArrayTypeError    = errors.New("Expected Type to be an Array.")
+	SyntaxError       = errors.New("Bad Syntax.")
+	NotSupportedError = errors.New("Not Supported")
+)
+
+func applyNext(nn node, v interface{}) (interface{}, error) {
+	if nn == nil {
+		return v, nil
+	}
+	return nn.Apply(v)
+}
+
+// A root type just has the NextNode which is it's self. If node is nil, we
+// Just return the interface, there is no need to filter.
+type RootNode struct {
+	NextNode node
+}
+
+func (r *RootNode) SetNext(n node) {
+	if r.NextNode == nil {
+		r.NextNode = n
+	}
+	r.NextNode.SetNext(n)
+}
+
+func (r *RootNode) Apply(v interface{}) (interface{}, error) {
+	return applyNext(r.NextNode, v)
+}
+
+type MapSelection struct {
+	Key string
+	RootNode
+}
+
+func (m *MapSelection) Apply(v interface{}) (interface{}, error) {
+	mv, ok := v.(map[string]interface{})
+	if !ok {
+		return v, MapTypeError
+	}
+	return applyNext(m.NextNode, mv[m.Key])
+}
+
+type ArraySelection struct {
+	Key int
+	RootNode
+}
+
+func (a *ArraySelection) Apply(v interface{}) (interface{}, error) {
+	arv, ok := v.([]interface{})
+	if !ok {
+		return v, ArrayTypeError
+	}
+	return applyNext(a.NextNode, arv[a.Key])
+}
+
+type WildCardSelection struct {
+	RootNode
+}
+
+func (w *WildCardSelection) Apply(v interface{}) (interface{}, error) {
+	switch tv := v.(type) {
+	case map[string]interface{}:
+		var ret []interface{}
+		for _, val := range tv {
+			rval, err := applyNext(w.NextNode, val)
+			// Don't add anything that causes an error or returns nil.
+			if err == nil || rval != nil {
+				ret = append(ret, rval)
+			}
+		}
+		return ret, nil
+	case []interface{}:
+		var ret []interface{}
+		for _, val := range tv {
+			rval, err := applyNext(w.NextNode, val)
+			// Don't add anything that causes an error or returns nil.
+			if err == nil || rval != nil {
+				ret = append(ret, rval)
+			}
+		}
+		return ret, nil
+
+	default:
+		return applyNext(w.NextNode, v)
+	}
+}
+
+type DescentSelection struct {
+	RootNode
+}
+
+func (d *DescentSelection) Apply(v interface{}) (interface{}, error) {
+	var ret []interface{}
+	rval, err := applyNext(d.NextNode, v)
+	if err != nil {
+		return nil, err
+	}
+	if rval != nil {
+		ret = append(ret, rval)
+	}
+	switch tv := v.(type) {
+	default:
+		return ret, nil
+	case map[string]interface{}:
+		for _, val := range tv {
+			rval, err := d.Apply(val)
+			// Don't add anything that causes an error or returns nil.
+			if err == nil || rval != nil {
+				ret = append(ret, rval)
+			}
+		}
+		return ret, nil
+	case []interface{}:
+		for _, val := range tv {
+			rval, err := d.Apply(val)
+			// Don't add anything that causes an error or returns nil.
+			if err == nil || rval != nil {
+				ret = append(ret, rval)
+			}
+		}
+		return ret, nil
+	}
+}
 
 // The first thing we need to do is transform a dot–notation to a bracket–notation.
 
@@ -14,9 +154,9 @@ func minNotNeg1(a int, bs ...int) int {
 	return m
 }
 
-func normalize(s string) (string, error) {
+func normalize(s string) string {
 	if s == "" || s == "$." {
-		return "$", nil
+		return "$"
 	}
 	r := "$"
 	// first thing to do is read passed the $
@@ -53,7 +193,6 @@ func normalize(s string) (string, error) {
 		}
 
 		n := minNotNeg1(strings.Index(s, "["), strings.Index(s, "."))
-
 		if n != -1 {
 			r += `["` + s[:n] + `"]`
 			s = s[n:]
@@ -62,5 +201,52 @@ func normalize(s string) (string, error) {
 			s = ""
 		}
 	}
-	return r, nil
+	return r
+}
+
+func getNode(s string) (node, string, error) {
+	var rs string
+	if len(s) == 0 {
+		return nil, s, io.EOF
+	}
+	n := strings.Index(s, "]")
+	if n == -1 {
+		return nil, s, SyntaxError
+	}
+	if len(s) > n {
+		rs = s[n+1:]
+	}
+	switch s[:2] {
+	case "[\"":
+		return &MapSelection{Key: s[2 : n-1]}, rs, nil
+	case "[*":
+		return &WildCardSelection{}, rs, nil
+	case "[.":
+		return &DescentSelection{}, rs, nil
+	case "[?", "[(":
+		return nil, rs, NotSupportedError
+	default: // Assume it's a array index otherwise.
+		i, err := strconv.Atoi(s[1:n])
+		if err != nil {
+			return nil, rs, SyntaxError
+		}
+		return &ArraySelection{Key: i}, rs, nil
+	}
+}
+
+func Parse(s string) (Applicator, error) {
+	var nn node
+	var err error
+	s = normalize(s)
+	rt := RootNode{}
+	// Remove the starting '$'
+	s = s[1:]
+	for len(s) > 0 {
+		nn, s, err = getNode(s)
+		if err != nil {
+			return nil, err
+		}
+		rt.SetNext(nn)
+	}
+	return &rt, nil
 }
--- parse_test.go
+++ parse_test.go
@@ -4,11 +4,9 @@ import "testing"
 
 func TestNormalize(t *testing.T) {
 	var ev string
-	var err error
 	testcases := []struct {
 		t                 string // Test Value
 		e                 string // Expected Value
-		err               error  // Expected Error
 		DontTestImplicate bool
 	}{
 		{t: "$.store.book[0].title", e: `$["store"]["book"][0]["title"]`},
@@ -19,26 +17,107 @@ func TestNormalize(t *testing.T) {
 		{t: "..author", e: `$[..]["author"]`, DontTestImplicate: true},
 		{t: "$.store.*", e: `$["store"][*]`},
 		{t: "$.store.book[?(@.length()-1)].title", e: `$["store"]["book"][?(@.length()-1)]["title"]`},
+		{t: "$.store.book[1:10:2].title", e: `$["store"]["book"][1:10:2]["title"]`},
 	}
 	for i, test := range testcases {
 		// First let's run the normal test.
-		ev, err = normalize(test.t)
-		if ev != test.e || err != test.err {
-			t.Errorf("[%03d] Standard test:  Normalize(\"%v\") = \"%v\", %v; want %v, %v", i, test.t, ev, err, test.e, test.err)
+		if ev = normalize(test.t); ev != test.e {
+			t.Errorf("[%03d] Standard test:  Normalize(\"%v\") = \"%v\"; want \"%v\"", i, test.t, ev, test.e)
 		}
 		// Test that a normalized version is a noop.
-		ev, err = normalize(test.e)
-		if ev != test.e || err != test.err {
-			t.Errorf("[%03d] NOOP test:      Normalize(\"%v\") = \"%v\", %v; want %v, %v", i, test.e, ev, err, test.e, test.err)
+		if ev = normalize(test.e); ev != test.e {
+			t.Errorf("[%03d] NOOP test:      Normalize(\"%v\") = \"%v\"; want \"%v\"", i, test.e, ev, test.e)
 		}
 		if !test.DontTestImplicate {
 			// Test that an implicate version works as well.
-			ev, err = normalize(test.t[2:])
-			if ev != test.e || err != test.err {
-				t.Errorf("[%03d] Implicate test: Normalize(\"%v\") = \"%v\", %v; want %v, %v", i, test.t[2:], ev, err, test.e, test.err)
+			if ev = normalize(test.t[2:]); ev != test.e {
+				t.Errorf("[%03d] Implicate test: Normalize(\"%v\") = \"%v\"; want \"%v\"", i, test.t[2:], ev, test.e)
 			}
 		}
-
 	}
+}
 
+func isSameArraySelectionNode(n *ArraySelection, m node) bool {
+	switch mv := m.(type) {
+	case *ArraySelection:
+		return mv.Key == n.Key
+	default:
+		return false
+	}
+}
+func isSameMapSelectionNode(n *MapSelection, m node) bool {
+	switch mv := m.(type) {
+	case *MapSelection:
+		return mv.Key == n.Key
+	default:
+		return false
+	}
+}
+func isSameRootNodeNode(n *RootNode, m node) bool {
+	switch m.(type) {
+	case *RootNode:
+		return true
+	default:
+		return false
+	}
+}
+func isSameWildcardSelectionNode(n *WildCardSelection, m node) bool {
+	switch m.(type) {
+	case *WildCardSelection:
+		return true
+	default:
+		return false
+	}
+}
+func isSameDescentSelectionNode(n *DescentSelection, m node) bool {
+	switch m.(type) {
+	case *DescentSelection:
+		return true
+	default:
+		return false
+	}
+}
+
+func isSameNode(n, m node) bool {
+	if n == nil && m == nil {
+		return true
+	}
+	switch nv := n.(type) {
+	case *MapSelection:
+		return isSameMapSelectionNode(nv, m)
+	case *ArraySelection:
+		return isSameArraySelectionNode(nv, m)
+	case *RootNode:
+		return isSameRootNodeNode(nv, m)
+	case *DescentSelection:
+		return isSameDescentSelectionNode(nv, m)
+	case *WildCardSelection:
+		return isSameWildcardSelectionNode(nv, m)
+	default:
+		return false
+	}
+}
+
+func TestGetNode(t *testing.T) {
+	testcases := []struct {
+		t   string
+		n   node
+		s   string
+		err error
+	}{
+		{t: `["store"]`, n: &MapSelection{Key: "store"}},
+		{t: `[10]`, n: &ArraySelection{Key: 10}},
+		{t: `[*]`, n: &WildCardSelection{}},
+		{t: `[..]`, n: &DescentSelection{}},
+		{t: `[?(@.lenght())]`, n: nil, err: NotSupportedError},
+		{t: `[(@.foo)]`, n: nil, err: NotSupportedError},
+		{t: `[0:10:2]`, n: nil, err: SyntaxError},
+	}
+	for i, test := range testcases {
+		n, s, err := getNode(test.t)
+		b := isSameNode(n, test.n)
+		if err != test.err || s != test.s || !b {
+			t.Errorf(`[%03d] getNode("%v") = %T, "%v","%v"; want %T, "%v", "%v"`, i, test.t, n, s, err, test.n, test.s, test.err)
+		}
+	}
 }
